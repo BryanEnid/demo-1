b0bd3cb49ae4e0f77d1304988cc20e436d03a5f1
bryanenid <bryanenid97@gmail.com>
2023-09-12 09:23:32 -0400

    Added PWA and HTTPS config files

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "obs-version-1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/src/App.css",
+  "/src/index.css",
+  // "/src/App.css",
+  // "/src/App.css",
+  // "/src/App.css",
+  // "/src/App.css",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(function () {
+          return caches.match("/offline.html"); // Serve offline.html when there's no network response
+        })
+      );
+    })
+  );
+});
