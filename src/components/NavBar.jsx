186bd892ab7e5c0c01c83438bdcbb34f86bd3001
Oleh Pylypenko <oleh.pylypenko@hygge.software>
2024-01-08 14:26:15 +0200

    Update Profile dropdown menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -119,41 +119,40 @@ export function NavBar({ createBucket }) {
 									Profile
 									<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
 								</DropdownMenuItem>
-								<DropdownMenuItem>
-									Billing
-									<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-								</DropdownMenuItem>
-								<DropdownMenuItem>
-									Settings
-									<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-								</DropdownMenuItem>
-								<DropdownMenuItem>
-									Keyboard shortcuts
-									<DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-								</DropdownMenuItem>
+								{/*<DropdownMenuItem>*/}
+								{/*	Billing*/}
+								{/*	<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>*/}
+								{/*</DropdownMenuItem>*/}
+								{/*<DropdownMenuItem>*/}
+								{/*	Settings*/}
+								{/*	<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>*/}
+								{/*</DropdownMenuItem>*/}
+								{/*<DropdownMenuItem>*/}
+								{/*	Keyboard shortcuts*/}
+								{/*	<DropdownMenuShortcut>⌘K</DropdownMenuShortcut>*/}
+								{/*</DropdownMenuItem>*/}
 							</DropdownMenuGroup>
 							<DropdownMenuSeparator />
 							<DropdownMenuGroup>
-								<DropdownMenuItem>Team</DropdownMenuItem>
-								<DropdownMenuSub>
-									<DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-									<DropdownMenuPortal>
-										<DropdownMenuSubContent>
-											<DropdownMenuItem>Email</DropdownMenuItem>
-											<DropdownMenuItem>Message</DropdownMenuItem>
-											<DropdownMenuSeparator />
-											<DropdownMenuItem>More...</DropdownMenuItem>
-										</DropdownMenuSubContent>
-									</DropdownMenuPortal>
-								</DropdownMenuSub>
+								{/*<DropdownMenuItem>Team</DropdownMenuItem>*/}
+								{/*<DropdownMenuSub>*/}
+								{/*	<DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>*/}
+								{/*	<DropdownMenuPortal>*/}
+								{/*		<DropdownMenuSubContent>*/}
+								{/*			<DropdownMenuItem>Email</DropdownMenuItem>*/}
+								{/*			<DropdownMenuItem>Message</DropdownMenuItem>*/}
+								{/*			<DropdownMenuSeparator />*/}
+								{/*			<DropdownMenuItem>More...</DropdownMenuItem>*/}
+								{/*		</DropdownMenuSubContent>*/}
+								{/*	</DropdownMenuPortal>*/}
+								{/*</DropdownMenuSub>*/}
 								<DropdownMenuItem>
-									New Team
+									New Organization
 									<DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
 								</DropdownMenuItem>
 							</DropdownMenuGroup>
 							<DropdownMenuSeparator />
-							<DropdownMenuItem>GitHub</DropdownMenuItem>
-							<DropdownMenuItem>Support</DropdownMenuItem>
+							<DropdownMenuItem>Feedback</DropdownMenuItem>
 							<DropdownMenuItem disabled>API</DropdownMenuItem>
 							<DropdownMenuSeparator />
 							<DropdownMenuItem onClick={handleLogOut}>
