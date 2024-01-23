9da46fdc567bcc9e0fce1c90eb5ad1812b414ce5
Bryan Enid <bryanenid97@gmail.com>
2024-01-23 10:20:46 -0500

    supports bucket linking

--- src/screens/profile/experience/History.jsx
+++ src/screens/profile/experience/History.jsx
@@ -1,7 +1,7 @@
 import { Card, CardHeader, CardContent } from '@/chadcn/Card';
 import { Typography } from '@/chadcn/Typography';
 import ConfirmDialog from '@/components/ConfirmDialog';
-import { getContrastColorAsync, getFirstPixelColor } from '@/lib/utils';
+import { getContrastColorAsync, getFirstPixelColor, isYouTubeUrl } from '@/lib/utils';
 import React from 'react';
 import { useExperience } from './useExperience';
 import { useProfile } from '@/hooks/useProfile';
@@ -12,6 +12,7 @@ import { Icon } from '@iconify/react';
 import { Calendar } from '@/chadcn/Calendar';
 import { Popover, PopoverContent, PopoverTrigger, PopoverClose } from '@/chadcn/Popover';
 import { Checkbox } from '@/chadcn/Checkbox';
+import { useBuckets } from '@/hooks/useBuckets';
 
 const FormatDate = ({ date: dateString }) => {
 	const [output, setOutput] = React.useState();
@@ -33,6 +34,7 @@ export const History = ({ title, data }) => {
 	// Hooks
 	const { isUserProfile, data: profile } = useProfile();
 	const { updateProfile } = useExperience();
+	const { data: buckets } = useBuckets(profile);
 
 	// State
 	const [history, setHistory] = React.useState([]);
@@ -45,9 +47,14 @@ export const History = ({ title, data }) => {
 	const [startDate, setStartDate] = React.useState();
 	const [endDate, setEndDate] = React.useState();
 	const [presentJob, setPresentJob] = React.useState(false);
+	const [linkedBucket, setLinkedBucket] = React.useState();
 
 	// Refs
 	const companySearchRef = React.useRef();
+	const bucketsSearchRef = React.useRef();
+
+	// Root level variables
+	const section = React.useMemo(() => String(title).toLowerCase().replace(' ', '-'), [title]);
 
 	React.useEffect(() => {
 		const fetchImageColors = async () => {
@@ -78,17 +85,23 @@ export const History = ({ title, data }) => {
 
 	// TODO: Handle form instead
 	const handleSave = () => {
-		const section = String(title).toLowerCase().replace(' ', '-');
-		const body = {
+		const newItem = {
 			title: jobTitle,
 			company: company.name,
 			companyLogoUrl: company.logo,
 			startDate,
 			endDate: presentJob ? null : endDate,
-			currentCompany: presentJob
+			currentCompany: presentJob,
+			bucketId: linkedBucket?.id
 		};
+		const payload = [...data, newItem];
 
-		updateProfile({ body, id: profile.uid, section });
+		if (section === 'certifications') {
+			newItem.provider = company.name;
+			newItem.providerLogoUrl = company.logo;
+		}
+
+		updateProfile({ body: payload, id: profile.uid, section });
 
 		clear();
 		setShow(false);
@@ -113,11 +126,26 @@ export const History = ({ title, data }) => {
 		setCompany(item);
 	};
 
+	const handleAddBucket = (item) => {
+		// bucketsSearchRef.current?.value = '';
+
+		setLinkedBucket(item);
+	};
+
+	const handleBucketSearch = ({ target }) => {};
+
 	const clear = () => {
+		setLinkedBucket(null);
 		setLogos([]);
 	};
 
-	if (!history.length) return <></>;
+	const handleSrc = (src, item) => {
+		if (isYouTubeUrl(src)) return item.videos[0].image;
+
+		return src;
+	};
+
+	if (!isUserProfile && !history) return;
 
 	return (
 		<>
@@ -214,7 +242,54 @@ export const History = ({ title, data }) => {
 						</div>
 
 						{/* Link to bucket */}
-						{/* <Input placeholder="Link bucket" /> */}
+						<Popover>
+							{section !== 'certifications' && (
+								<PopoverTrigger className="flex flex-row mt-5 items-center gap-2">
+									<Button variant="secondary">Link to a bucket</Button>
+									{linkedBucket && 'Linked bucket: ' + linkedBucket.name}
+								</PopoverTrigger>
+							)}
+
+							<PopoverContent className="w-full p-2" align="start">
+								<div className="w-[454px] flex flex-col gap-2">
+									{/* <Input ref={bucketsSearchRef} placeholder="Bucket name" onChange={handleBucketSearch} /> */}
+
+									<div className="z-10 bg-white">
+										{!!buckets?.length && (
+											<div>
+												<div
+													className="flex flex-col gap-1 p-2 w-full border-t-0 z-10"
+													style={{ borderRadius: '0px 0px 0.75rem 0.75rem' }}
+												>
+													{buckets.map((item) => {
+														// if (!item.videos?.[0]?.image) return;
+														return (
+															<PopoverClose key={item.name}>
+																<Button
+																	className="w-full justify-start px-8 h-auto"
+																	variant="ghost"
+																	onClick={() => handleAddBucket(item)}
+																>
+																	{item.videos?.[0]?.image && (
+																		<img
+																			src={item.videos?.[0]?.image}
+																			className="aspect-square w-10 object-cover inline"
+																		/>
+																	)}
+																	<Typography variant="p" className="px-3 inline">
+																		{item.name}
+																	</Typography>
+																</Button>
+															</PopoverClose>
+														);
+													})}
+												</div>
+											</div>
+										)}
+									</div>
+								</div>
+							</PopoverContent>
+						</Popover>
 					</div>
 				</div>
 
@@ -237,9 +312,7 @@ export const History = ({ title, data }) => {
 
 			<div>
 				<div className="flex flex-row items-center mb-5 gap-2">
-					<Typography variant="h2" className="mb-5">
-						{title}
-					</Typography>
+					<Typography variant="h2">{title}</Typography>
 
 					{isUserProfile && (
 						<button onClick={() => setShow(true)}>
@@ -249,30 +322,70 @@ export const History = ({ title, data }) => {
 				</div>
 
 				<div className="flex flex-col gap-5">
-					{history.map(({ title, company, companyLogoUrl, bgColor, textColor, startDate, endDate }, index) => (
-						<div key={index}>
-							<Card className={`grid grid-cols-5 py-5 `} style={{ background: bgColor, color: textColor }}>
-								<CardHeader className="flex justify-center items-center">
-									<img src={companyLogoUrl} className="aspect-square object-contain w-20" />
-								</CardHeader>
+					{history.map(
+						(
+							{ title, company, companyLogoUrl, bgColor, textColor, startDate, endDate, currentCompany, bucketId },
+							index
+						) => {
+							const bucket = buckets?.find(({ id }) => id === bucketId);
+							const previewSrc = bucket?.videos?.[0]?.videoUrl;
 
-								<CardContent className={`flex flex-col justify-center p-0 col-span-3`}>
-									<Typography variant="p" className="font-bold">
-										{title}
-									</Typography>
+							return (
+								<div key={index}>
+									<Card className={`grid grid-cols-5 py-5 `} style={{ background: bgColor, color: textColor }}>
+										<CardHeader className="flex justify-center items-center">
+											<img src={companyLogoUrl} className="aspect-square object-contain w-20" />
+										</CardHeader>
 
-									<Typography variant="p">{company}</Typography>
-									<Typography variant="p">
-										<FormatDate date={startDate} /> - <FormatDate date={endDate} />
-									</Typography>
-								</CardContent>
+										<CardContent className={`flex flex-col justify-center p-0 col-span-3`}>
+											<Typography variant="p" className="font-bold">
+												{title}
+											</Typography>
 
-								{/* <CardFooter className="flex justify-center items-center p-0">
-					<p>Card Footer</p>
-				</CardFooter> */}
-							</Card>
+											<Typography variant="p">{company}</Typography>
+											<Typography variant="p">
+												<FormatDate date={startDate} /> - {currentCompany ? 'Present' : <FormatDate date={endDate} />}
+											</Typography>
+										</CardContent>
+
+										{section !== 'certifications' && (
+											<div className="flex justify-center items-center p-0">
+												{/* {JSON.stringify(buckets?.find(({ id }) => id === bucketId)?.name, null, 2)} */}
+
+												<div className="px-7">
+													{isYouTubeUrl(previewSrc) ? (
+														<img
+															className="aspect-square object-cover rounded-2xl h-full "
+															src={handleSrc(previewSrc, bucket)}
+														/>
+													) : (
+														<video
+															type="video/mp4"
+															autoPlay
+															muted
+															loop
+															className="aspect-square object-cover rounded-2xl h-full "
+															src={handleSrc(previewSrc, bucket)}
+														/>
+													)}
+												</div>
+											</div>
+										)}
+									</Card>
+								</div>
+							);
+						}
+					)}
+
+					{!history.length && isUserProfile && (
+						<div className="rounded-xl p-10 border-dashed border-2 border-primary flex flex-col text-center text-slate-500">
+							Customize your profile by listing the talents and expertise that define you. A modal will pop up, allowing
+							you to effortlessly manage and showcase your skills. Let your strengths shine!
+							<Button variant="link" onClick={() => setShow(true)}>
+								🌟 Click on this link to add your skills.
+							</Button>
 						</div>
-					))}
+					)}
 				</div>
 			</div>
 		</>
