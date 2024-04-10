73a6f3319f2c2b776216d3d5ab8609024f2ae92b
Bryan Enid <BryanEnid97@gmail.com>
2024-04-09 22:27:47 -0400

    still working (#121)

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -14,6 +14,43 @@ import { ImageCacheProvider } from './ImageCacheProvider';
 export function GlobalProvider({ children }) {
 	const { isMobile } = useMobile();
 
+	React.useEffect(() => {
+		const graphs = {
+			development: `██████╗ ███████╗██╗   ██╗
+			██╔══██╗██╔════╝██║   ██║
+			██║  ██║█████╗  ██║   ██║
+			██║  ██║██╔══╝  ╚██╗ ██╔╝
+			██████╔╝███████╗ ╚████╔╝`,
+			production: `██████╗ ██████╗ ██████╗ 
+			██╔══██╗██╔══██╗██╔══██╗
+			██████╔╝██████╔╝██║  ██║
+			██╔═══╝ ██╔══██╗██║  ██║
+			██║     ██║  ██║██████╔╝
+			╚═╝     ╚═╝  ╚═╝╚═════╝ `
+		};
+		const mode = graphs[import.meta.env.MODE];
+
+		// ANSI escape codes for styling
+		const styles = {
+			bold: '\x1b[1m',
+			underline: '\x1b[4m',
+			red: '\x1b[31m',
+			green: '\x1b[32m',
+			reset: '\x1b[0m' // Reset to default styling
+		};
+
+		// Define styling for the text
+		const headerStyle = `${styles.bold}${styles.underline}${styles.red}`;
+		const modeStyle = `${styles.bold}${styles.green}`;
+
+		console.log(`
+ 			The application is running in 
+			${modeStyle}${mode}${styles.reset} mode.
+			
+			Back end url: ${import.meta.env.VITE_SERVER_BASE_URL}
+		`);
+	}, []);
+
 	return (
 		<ReactQueryProvider>
 			<ImageCacheProvider>
